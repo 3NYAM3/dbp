6c0905eea6cebf59bca127955ff7c48a21404020
yponion <yp071704@naver.com>
2023-12-01 18:43:22 +0900

    LoginPage

--- src/main/frontend/src/Pages/LoginPage.js
+++ src/main/frontend/src/Pages/LoginPage.js
@@ -1,6 +1,31 @@
 const LoginPage = () => {
     return (
-        <div>LoginPage</div>
+        <div className="create-container">
+            <div>
+                <input placeholder="아이디"/>
+                <input placeholder="비밀번호"/>
+                <input type="submit"
+                       onClick={()=>{
+                           //클릭 구현
+                       }}
+                />
+            </div>
+            <div>
+                <input placeholder="이름"/>
+                <input placeholder="주민앞6자리"/>
+                <input placeholder="주민뒤1자리"/>
+                <input placeholder="아이디"/>
+                <input placeholder="비밀번호"/>
+                <input placeholder="비밀번호 확인"/>
+                <input placeholder="이메일"/>
+                <input placeholder="전화번호"/>
+                <input type="submit"
+                       onClick={()=>{
+                            //클릭 구현
+                       }}
+                />
+            </div>
+        </div>
     );
 };
 export default LoginPage
