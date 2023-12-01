481a690921a58978932abfe996b8aa9107a4e718
yponion <yp071704@naver.com>
2023-12-01 22:00:24 +0900

    LoginPage.js

--- src/main/frontend/src/Pages/LoginPage.js
+++ src/main/frontend/src/Pages/LoginPage.js
@@ -1,18 +1,19 @@
 import {useState} from "react";
+import {useDispatch} from "react-redux";
+import {login} from "../store/authSlice";
+import {useNavigate} from "react-router-dom";
 
 const LoginPage = () => {
-    const [loginId, setLoginId] = useState();
+    const [loginEmail, setLoginEmail] = useState();
     const [loginPassword, setLoginPassword] = useState();
 
     const [name, setName] = useState();
-    const [frontNumber, setFrontNumber] = useState();
-    const [endNumber, setEndNumber] = useState();
-    const [id, setId] = useState();
+    const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const [passwordCheck, setPasswordCheck] = useState();
-    const [email, setEmail] = useState();
-    const [phoneNumber, setPhoneNumber] = useState();
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     return (
         <div className="login-box">
@@ -20,13 +21,15 @@ const LoginPage = () => {
                 <div className="form-container sign-in">
                     <form>
                         <h1>로그인</h1>
-                        <input type="text" placeholder="아이디" required={true}
-                               value={loginId} onChange={(e) => setLoginId(e.target.value)}/>
+                        <input type="email" placeholder="이메일" required={true}
+                               value={loginEmail} onChange={(e) => setLoginEmail(e.target.value)}/>
                         <input type="password" placeholder="비밀번호" required={true}
                                value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)}/>
                         <button
                             onClick={()=>{
                                 // 로그인 로직
+                                dispatch(login());
+                                navigate('/');
                             }}
                         >로그인</button>
                     </form>
@@ -36,20 +39,12 @@ const LoginPage = () => {
                         <h1>회원가입</h1>
                         <input type="text" placeholder="이름" required={true}
                                value={name} onChange={(e) => setName(e.target.value)}/>
-                        <input type="number" placeholder="주민번호 앞 6자리" required={true}
-                               value={frontNumber} onChange={(e) => setFrontNumber(e.target.value)}/>
-                        <input type="number" placeholder="주민번호 뒷 1자리" required={true}
-                               value={endNumber} onChange={(e) => setEndNumber(e.target.value)}/>
-                        <input type="text" placeholder="아이디" required={true}
-                               value={id} onChange={(e) => setId(e.target.value)}/>
+                        <input type="email" placeholder="이메일" required={true}
+                               value={email} onChange={(e) => setEmail(e.target.value)}/>
                         <input type="password" placeholder="비밀번호" required={true}
                                value={password} onChange={(e) => setPassword(e.target.value)}/>
                         <input type="password" placeholder="비밀번호 확인" required={true}
                                value={passwordCheck} onChange={(e) => setPasswordCheck(e.target.value)}/>
-                        <input type="email" placeholder="이메일" required={true}
-                               value={email} onChange={(e) => setEmail(e.target.value)}/>
-                        <input type="number" placeholder="전화번호" required={true}
-                               value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}/>
                         <button
                             onClick={()=>{
                                 // 회원가입 로직
