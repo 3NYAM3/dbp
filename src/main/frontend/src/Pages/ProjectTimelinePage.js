edd31c0ef7d759f5c09ac060676cac75196dc96d
yponion <yp071704@naver.com>
2023-12-19 04:24:12 +0900

    타임라인 날짜선

--- src/main/frontend/src/Pages/ProjectTimelinePage.js
+++ src/main/frontend/src/Pages/ProjectTimelinePage.js
@@ -28,16 +28,32 @@ const ProjectTimelinePage = () => {
         })
     }, []);
 
-    const oneDayTo10px = (input) => { // 1일당 10px
-        return (
-            (new Date(input) / (1000 * 60 * 60 * 24)) * 10
-        )
+
+    const oneDayTo10px = (input, scaleFactor = 1) => {
+        const dif = new Date(projectEnd) / (1000 * 60 * 60 * 24) - new Date(projectStart) / (1000 * 60 * 60 * 24);
+        // console.log(dif)
+        // scaleFactor = 150 / dif
+        return ((new Date(input) - new Date(projectStart)) / (1000 * 60 * 60 * 24)) * 10 * scaleFactor;
     }
+
 
     const ps = oneDayTo10px(projectStart); // 프로젝트 시작
     const pe = oneDayTo10px(projectEnd); // 프로젝트 끝
     const pl = pe - ps; // 프로젝트 길이
 
+    const calculateMonthlyMarkers = (startDate, endDate) => {
+        let markers = [];
+        let currentDate = new Date(startDate);
+        currentDate.setDate(1); // 매달 첫째 날로 설정
+
+        while (currentDate <= new Date(endDate)) {
+            markers.push(new Date(currentDate));
+            currentDate.setMonth(currentDate.getMonth() + 1);
+        }
+
+        return markers;
+    }
+    const monthlyMarkers = calculateMonthlyMarkers(projectStart, projectEnd);
 
     return (
         <>
@@ -46,6 +62,19 @@ const ProjectTimelinePage = () => {
                 <p>{projectStart} ~ {projectEnd}</p>
             </div>
             <div className="container-timeline">
+                {calculateMonthlyMarkers(projectStart, projectEnd).map((date, index) => (
+                    <div
+                        className="timeline-month-marker"
+                        key={index}
+                        style={{
+                            left: `${oneDayTo10px(date) - ps}px`,
+                            top: 0, // 선이 상단에 고정되도록 설정
+                            height: '100%' // 선이 컨테이너 높이를 전부 차지하도록 설정
+                        }}
+                    >
+                        <span className="timeline-date-label">{date.toLocaleDateString()}</span>
+                    </div>
+                ))}
                 {taskList.map((task, index) => {
 
                     const ts = oneDayTo10px(task.startDate) - ps; // 작업 시작
